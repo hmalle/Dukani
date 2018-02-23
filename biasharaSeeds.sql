401a098df817d5075f47593c5c2e36fe9cdf5497
hmalle <hmalle90ak@gmail.com>
2018-02-23 01:05:43 -0800

    displaying all the items in the database

--- biasharaSeeds.sql
+++ biasharaSeeds.sql
@@ -7,9 +7,13 @@ USE biasharaDB;
 
 CREATE TABLE products (
   id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(100) NOT NULL,
-  department_name VARCHAR(100) NOT NULL,
+  product VARCHAR(100) NOT NULL,
+  department VARCHAR(100) NOT NULL,
   price DECIMAL(10,2) NOT NULL,
-  stock_quantity INT NOT NULL,
+  stock INT NOT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO  products(product, department, price, stock) 
+VALUES("A super computer", "Electronics", 999.00,10);
+
--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var columnify = require("columnify");
 
+var connection = mysql.createConnection({
+  host:  "127.0.0.1",
+  port: 3306,
+  user: "root",
+  password:"",
+  database:"biasharaDB"
+});
 
+connection.connect(function(error){
+  if(error) {
+    return console.log("Connection error " +error);
+  }else{
+    console.log("Connected , id "+connection.threadId+"\n");
+    var queryString = "SELECT * FROM products";
+    displayItems(queryString);
+  }
+});
+
+function displayItems( queryString ){
+  console.log("All Items available for sell");
+  connection.query( queryString, function(error,res){
+    if(error){
+      console.log("Error querying database: "+error);
+    }else{
+      console.log(columnify(res));
+    }
+  });
+}
+
+//connection.end() //TODO: to end connection
