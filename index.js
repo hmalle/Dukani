68f51a9ca69ea6757fb154d0de458e06b5ba99d1
hmalle <hmalle90ak@gmail.com>
2018-02-23 18:21:18 -0800

    The buyes can no go crazy buying things

--- index.js
+++ index.js
@@ -22,14 +22,57 @@ connection.connect(function(error){
 });
 
 function displayItems( queryString ){
-  console.log("All Items available for sell");
+  //display items based on the query supplied! 
   connection.query( queryString, function(error,res){
     if(error){
-      console.log("Error querying database: "+error);
+      console.log(error);
     }else{
       console.log(columnify(res));
     }
+    console.log("-------------------------------------------------------------------------------");
+    askUser();
   });
 }
 
-//connection.end() //TODO: to end connection
+function askUser(){
+  //prompts the user to enter the id of the item and the quantity to buy! 
+  var questions = [
+  {
+    name: "id",
+    message: "Enter the id of the product you would like to buy"
+  },{
+    name: "quantity",
+    message: "How many do you want ?"
+  }];
+  inquirer.prompt(questions).then( function(product){
+    //console.log(JSON.stringify(product));
+    var queryString = "SELECT * FROM products WHERE id="+ product.id;
+    var newStock=0;
+    connection.query(queryString, function(error, response){
+      if(!error){
+        if( parseInt(response[0].stock)<= 0 ){
+          console.log("\nThe "+response[0].product+" is out of stock! Please check again later\n");
+        }else if(product.quantity>parseInt(response[0].stock)){ 
+          console.log("\nInsufficient quantity to fulfil your demands.Please choose a different amount\n");
+        }else{
+          console.log("\nYou bought "+response[0].product+", Nice doing business with you\n");
+          newStock = parseInt(response[0].stock) - product.quantity;
+          updateProduct(response[0].id, newStock);
+        }
+      }else{
+        return console.log(error);
+      }
+      queryString = "SELECT * FROM products"; 
+      displayItems(queryString);
+    });
+  });
+}
+
+function updateProduct(id,newStock){
+  var queryString = "UPDATE products SET stock="+newStock+" WHERE id="+id;
+  connection.query(queryString,function(error, response){
+    if(error){ 
+      return console.log(error);
+    }
+  });
+}
