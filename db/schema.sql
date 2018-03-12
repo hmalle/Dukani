b77813770b0b007735e79672e81d51d0fe8c758d
hmalle <hmalle90ak@gmail.com>
2018-03-11 22:40:12 -0700

    created a db folder

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+
+DROP DATABASE IF EXISTS biasharaDB;
+
+CREATE DATABASE biasharaDB;
+
+USE biasharaDB;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  product VARCHAR(100) NOT NULL,
+  department VARCHAR(100) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
