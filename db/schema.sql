fc8a74a04b7e5c8c97d7c12b156600be5c334ce4
hmalle <hmalle90ak@gmail.com>
2018-04-10 19:13:21 -0700

    changed the schema and seed file by adding overhead costs to the products table

--- db/schema.sql
+++ db/schema.sql
@@ -11,6 +11,12 @@ CREATE TABLE products (
   department VARCHAR(100) NOT NULL,
   price DECIMAL(10,2) NOT NULL,
   stock INT NOT NULL,
+  product_sales INT NULL,
   PRIMARY KEY (id)
 );
 
+CREATE TABLE departments(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(100) NOT NULL,
+  over_head_costs INT NOT NULL DEFAULT 100
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,10 @@
 
-INSERT INTO  products(product, department, price, stock) 
-VALUES("A super computer", "Electronics", 999.00,10);
-
-INSERT INTO  products(product, department, price, stock) 
-VALUES("A super computer", "Electronics", 999.00,10);
-
-INSERT INTO  products(product, department, price, stock) 
-VALUES("A super computer", "Electronics", 999.00,10);
-
-INSERT INTO  products(product, department, price, stock) 
-VALUES("A super computer", "Electronics", 999.00,10);
-
-INSERT INTO  products(product, department, price, stock) 
-VALUES("A super computer", "Electronics", 999.00,10);
-
-INSERT INTO  products(product, department, price, stock) 
-VALUES("A super computer", "Electronics", 999.00,10);
-
-INSERT INTO  products(product, department, price, stock) 
-VALUES("A super computer", "Electronics", 999.00,10);
-
-INSERT INTO  products(product, department, price, stock) 
-VALUES("A super computer", "Electronics", 999.00,10);
-
+INSERT INTO  products(product, department, price, stock) VALUES
+("Qubit", "Electronics", 999.00,10),
+("Apple Watch", "Electronics", 400.00, 40),
+("Cards Against Humanity", "Toys", 20.00,100),
+("Table Saw","Construction",800.99, 3),
+("Drying Lotion","Skin Care", 145.99, 30),
+("Instant Pot", "Kitchen", 99.00,10),
+("Warehouse 13", "Movies", 43.99,122),
+("Scrimmage Soccer Vest", "Sports", 28.50,48);
